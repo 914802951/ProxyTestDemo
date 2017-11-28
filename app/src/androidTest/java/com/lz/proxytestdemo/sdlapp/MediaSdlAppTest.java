bc24d4c33781b2d926d0bb2f1650db9ff45e88c0
lizhi <914802951@qq.com>
2017-11-28 15:31:13 +0800

    add test case

--- app/src/androidTest/java/com/lz/proxytestdemo/sdlapp/MediaSdlAppTest.java
+++ app/src/androidTest/java/com/lz/proxytestdemo/sdlapp/MediaSdlAppTest.java
@@ -1,17 +1,98 @@
 package com.lz.proxytestdemo.sdlapp;
 
+import com.smartdevicelink.proxy.RPCRequest;
+import com.smartdevicelink.proxy.rpc.enums.UpdateMode;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 /**
  * Created by Administrator on 2017/11/23.
  */
 public class MediaSdlAppTest {
 
-//    @Before
-//    public void setUp() {
-//        MockitoAnnotations.initMocks(this);
-//    }
-//
-//    @Test
-//    private void SetMediaClockTimer(){
-//        MediaSdlApp app = Mockito.mock(MediaSdlApp.class);
-//    }
+    @Test
+    public void setHmiMediaClockTimer(){
+        Method method = null;
+        try {
+            MediaSdlApp app = Mockito.mock(MediaSdlApp.class);
+            Class[] parameterTypes = {Integer.class, Integer.class, UpdateMode.class};
+            method = MediaSdlApp.class.getDeclaredMethod("SetHmiMediaClockTimer", parameterTypes);
+            method.setAccessible(true);
+
+            Object[] arguments = new Object[]{null, null, UpdateMode.COUNTUP};
+            method.invoke(app, arguments);
+            verify(app, never()).sendRpcMsg(any(RPCRequest.class));
+
+            arguments = new Object[]{0, null, UpdateMode.COUNTUP};
+            method.invoke(app, arguments);
+            verify(app).sendRpcMsg(any(RPCRequest.class));
+        }catch (Exception e){
+            e.printStackTrace();
+            fail();
+        }finally {
+            if (method != null) {
+                method.setAccessible(false);
+            }
+        }
+    }
+
+    @Test
+    public void checkMediaClockTimer() {
+        int max = 59 * 60 * 60;
+        Method method = null;
+        try {
+            MediaSdlApp app = Mockito.mock(MediaSdlApp.class);
+            Class[] parameterTypes = {Integer.class, Integer.class, UpdateMode.class};
+            method = MediaSdlApp.class.getDeclaredMethod("checkMediaClockTimer", parameterTypes);
+            method.setAccessible(true);
+
+            Object[] arguments = new Object[]{null, null, UpdateMode.COUNTUP};
+            boolean result = (boolean) method.invoke(app, arguments);
+            assertEquals(false, result);
+
+            arguments = new Object[]{-1, null, UpdateMode.COUNTUP};
+            result = (boolean) method.invoke(app, arguments);
+            assertEquals(false, result);
+
+            arguments = new Object[]{max + 1, null, UpdateMode.COUNTUP};
+            result = (boolean) method.invoke(app, arguments);
+            assertEquals(false, result);
+
+            arguments = new Object[]{0, null, UpdateMode.COUNTUP};
+            result = (boolean) method.invoke(app, arguments);
+            assertEquals(true, result);
+
+            arguments = new Object[]{0, 10, UpdateMode.COUNTUP};
+            result = (boolean) method.invoke(app, arguments);
+            assertEquals(true, result);
+
+            arguments = new Object[]{20, 10, UpdateMode.COUNTUP};
+            result = (boolean) method.invoke(app, arguments);
+            assertEquals(false, result);
+
+            arguments = new Object[]{20, 10, UpdateMode.COUNTDOWN};
+            result = (boolean) method.invoke(app, arguments);
+            assertEquals(true, result);
+
+            arguments = new Object[]{null, 10, UpdateMode.PAUSE};
+            result = (boolean) method.invoke(app, arguments);
+            assertEquals(true, result);
+        }catch (Exception e){
+            e.printStackTrace();
+            fail();
+        }finally {
+            if (method != null) {
+                method.setAccessible(false);
+            }
+        }
+    }
 }
